e91c001d4817f8efd93ecb966c09a47cf1736814
GyHuba <gy.huba@gmail.com>
2023-10-17 21:22:48 +0200

    landing component added

---
+++ src/components/LandingPageComponent.jsx
@@ -0,0 +1,17 @@
+import "../css/landing-component.css";
+
+export default function LandingPageComponent(){
+
+    return(
+        <>
+        <div className="container content">
+            <div className="title">
+                Hi there, <br />
+                My name is Huba Gyihor,<br /> I am a junior <span className="important">frontend developer</span>, <br />
+                passionate about programming. <br />
+                Take a look around!
+            </div>
+        </div>
+        </>
+    )
+}
